fcbe42fd28409a3c3b7733289f6bfd6bf1d44f69
Paloma <palososa_19@hotmail.es>
2022-09-27 18:26:20 -0300

    commit

---
+++ EjerciciosPrgrm2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosPrgrm2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Primer ejercicio
+            //string palabra;
+
+            //Queue<string> colaPalabras = new Queue<string>();
+            //palabra = Console.ReadLine();
+
+            //while (palabra != "")
+            //{
+            //    colaPalabras.Enqueue(palabra);
+            //    Console.WriteLine("Ingrese la proxima palabra");
+            //    palabra = Console.ReadLine();
+            //}
+            //while (colaPalabras.Count > 0)
+            //{ 
+            //    Console.WriteLine(colaPalabras.Dequeue());
+            //}
+
+            //Console.ReadKey();
+            #endregion
+
+            #region Segundo ejercicio 
+            //string palabra;
+
+            //Stack<string> colaPalabras = new Stack<string>();
+            //palabra = Console.ReadLine();
+
+            //while (palabra != "")
+            //{
+            //    colaPalabras.Push(palabra);
+            //    Console.WriteLine("Ingrese la proxima palabra");
+            //    palabra = Console.ReadLine();
+            //}
+            //while (colaPalabras.Count > 0)
+            //{
+            //    Console.WriteLine(colaPalabras.Pop());
+            //}
+
+            //Console.ReadKey();
+
+            #endregion
+            #region Tercer ejercicio
+
+            string palabra;
+            int numero;
+            List<string> Lista = new List<string>();
+            palabra = Console.ReadLine();
+
+
+            while (palabra != "")
+            {
+                Lista.Add(palabra);
+                Console.WriteLine("Ingrese la proxima palabra");
+                palabra = Console.ReadLine();
+            }
+            Console.WriteLine("Ingrese la posición de la palabra que desea");
+
+            numero = Convert.ToInt32(Console.ReadLine());
+            while (numero != 0)
+            {
+                numero--;
+                Console.WriteLine(Lista[numero]);
+                numero = Convert.ToInt32(Console.ReadLine());
+            }
+
+            #endregion
+        }
+    }
+}
